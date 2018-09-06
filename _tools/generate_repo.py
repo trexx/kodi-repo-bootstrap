c707eec9c608ebac4d221bdd1b12e1ff2f1c875f
mammo0 <marc.ammon@hotmail.de>
2018-09-06 03:45:03 +0200

    converted to python3

--- _tools/generate_repo.py
+++ _tools/generate_repo.py
@@ -17,11 +17,11 @@ import zipfile
 import shutil
 import datetime
 from xml.dom import minidom
-from ConfigParser import SafeConfigParser
+from configparser import ConfigParser
 
 
 # Load the configuration:
-config = SafeConfigParser()
+config = ConfigParser()
 config.read('config.ini')
 tools_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__))))
 output_path = "_" + config.get('locations', 'output_path')
@@ -64,7 +64,7 @@ class Generator:
         if os.path.isfile(addonid + os.path.sep + "addon.xml"):
             return
 
-        print "Create repository addon"
+        print("Create repository addon")
 
         with open(tools_path + os.path.sep + "template.xml", "r") as template:
             template_xml = template.read()
@@ -107,11 +107,11 @@ class Generator:
                     version = parent.getAttribute("version")
                     addonid = parent.getAttribute("id")
                 self._generate_zip_file(addon, version, addonid)
-            except Exception, e:
-                print e
+            except Exception as e:
+                print(e)
 
     def _generate_zip_file(self, path, version, addonid):
-        print "Generate zip file for " + addonid + " " + version
+        print("Generate zip file for " + addonid + " " + version)
         filename = path + "-" + version + ".zip"
         try:
             zip = zipfile.ZipFile(filename, 'w', compression=zipfile.ZIP_DEFLATED)
@@ -134,11 +134,11 @@ class Generator:
                 os.rename(output_path + addonid + os.path.sep + filename,
                     output_path + addonid + os.path.sep + filename + "." + datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
             shutil.move(filename, output_path + addonid + os.path.sep + filename)
-        except Exception, e:
-            print e
+        except Exception as e:
+            print(e)
 
     def _generate_addons_file(self):
-        print "Generating addons.xml file"
+        print("Generating addons.xml file")
         # addon list
         addons = os.listdir(".")
         # final addons text
@@ -161,35 +161,35 @@ class Generator:
                     if (line.find("<?xml") >= 0):
                         continue
                     # add line
-                    addon_xml += unicode(line.rstrip() + "\n", "utf-8")
+                    addon_xml += line.rstrip() + "\n", "utf-8"
                 # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
-            except Exception, e:
+            except Exception as e:
                 # missing or poorly formatted addon.xml
-                print "Excluding %s for %s" % (_path, e,)
+                print("Excluding %s for %s" % (_path, e))
         # clean and add closing tag
         addons_xml = addons_xml.strip() + u"\n</addons>\n"
         # save file
         self._save_file(addons_xml.encode("utf-8"), file=output_path + "addons.xml")
 
     def _generate_md5_file(self):
-        print "Generating addons.xml.md5 file"
+        print("Generating addons.xml.md5 file")
         try:
             # create a new md5 hash
             m = hashlib.md5(open(output_path + "addons.xml").read()).hexdigest()
             # save file
             self._save_file(m, file=output_path + "addons.xml.md5")
-        except Exception, e:
+        except Exception as e:
             # oops
-            print "An error occurred creating addons.xml.md5 file!\n%s" % (e,)
+            print("An error occurred creating addons.xml.md5 file!\n%s" % e)
 
     def _save_file(self, data, file):
         try:
             # write data to the file
             open(file, "w").write(data)
-        except Exception, e:
+        except Exception as e:
             # oops
-            print "An error occurred saving %s file!\n%s" % (file, e,)
+            print("An error occurred saving %s file!\n%s" % (file, e))
 
 
 class Copier:
@@ -233,4 +233,4 @@ class Copier:
 if __name__ == "__main__":
     Generator()
     Copier()
-    print "Finished updating addons xml & md5 files, zipping addons and copying additional files"
+    print("Finished updating addons xml & md5 files, zipping addons and copying additional files")
